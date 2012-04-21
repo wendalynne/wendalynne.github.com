d3287324c53c482f4680cfdaa2989f2ccd673b68
Wendy Lin <linwendy08@gmail.com>
2012-04-21 16:03:14 -0700

    added js

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,1 +1,8 @@
-// Your Javascript will go here!
+$(document).ready(function() {
+   //$("#copyright").click(function(){
+   //     $(this).css('color','purple');
+   //});
+    document.getElementById('copyright').onclick = function () {
+        this.style.color='blue';
+    }
+});
